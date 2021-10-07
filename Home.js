91555dccd9504a9c32ec228f1a36c2a2c8ce5c81
Jatin <ym.jatin@gmail.com>
2021-10-07 21:22:42 +0530

    Home_Responsive_v3

--- Home.js
+++ Home.js
@@ -1,15 +1,30 @@
 
-const c_heading = document.querySelectorAll('.c_heading');
+const cHeading = document.querySelectorAll('.c_heading');
+const cBody1 = document.querySelectorAll('.c_body');
 
-c_heading.forEach(c_heading => {
-  c_heading.addEventListener('click' , event => {
-    c_heading.classList.toggle('active');
-    const c_body = c_heading.nextElementSibling;
-    if(c_heading.classList.contains('active')){
-      c_body.style.maxHeight = c_body.scrollHeight + 'px';
-    }
-    else {
-      c_body.style.maxHeight = 0;
+cHeading.forEach(cHeading => {
+  cHeading.addEventListener('click', event => {
+    cHeading.classList.toggle('active');
+    const cBody = cHeading.nextElementSibling;
+    if (cHeading.classList.contains('active')) {
+      cBody.style.maxHeight = cBody.scrollHeight + 'px';
+    } else {
+      cBody.style.maxHeight = 0;
     }
   });
 });
+
+// VideoPlayer
+var videoPlayer = document.getElementById('videoPlayer');
+var myVideo = document.getElementById('myVideo');
+
+function stopVideo() {
+  videoPlayer.style.display = 'none';
+}
+
+function playVideo(file) {
+  myVideo.src = file;
+  videoPlayer.style.display = 'block';
+}
+
+// VideoPlayer
